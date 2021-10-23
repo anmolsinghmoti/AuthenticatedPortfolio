402e66edc49e4e62a95ce6b1425e505c8f64bfc4
Anmol Singh <anmolsinghmoti@gmail.com>
2021-10-22 20:44:02 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports={
+
+   "URI":"mongodb://localhost/contacts"
+   // "URI":"mongodb+srv://Anmol:YHB2g9DWfLy0kB33@mongodbserver.amhnd.mongodb.net/ContactsDb?retryWrites=true&w=majority"
+}
---
+++ server/routes/users.js
@@ -0,0 +1,14 @@
+/* FileName : index.js */
+/* StudentName :Anmol Singh */
+/* Student Id :301145362 */
+/* Date : 09/22/2021 */
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Annie');
+});
+
+module.exports = router;
